d1f82c6fa447aa13427dd2e15f05e3d6ca2f02c6
SeconDatke <secondatke@secondwtq.local>
2014-08-01 01:36:37 +0800

    Initial Commit

---
+++ MarXsCube/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MarXsCube
+//
+//  Created by SeconDatke on 14-8-1.
+//  Copyright (c) 2014年 MarXsCube Staff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
