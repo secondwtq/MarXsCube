b2e7885917703791470ede9a3a9770d36f4b965b
secondwtq <lovejay-lovemusic@outlook.com>
2014-12-14 18:46:21 +0800

    Moved XLauncher to MarKsCube_build repo.

--- XLauncher.app/Contents/Resources/Scripts/Debug.lua
+++
@@ -1,5 +0,0 @@
-Debug = { }
-
-function Debug.PrintMouseStatus(mouseStatus)
-	print(mouseStatus.x, mouseStatus.y, mouseStatus.left, mouseStatus.middle, mouseStatus.right)
-end
--- XLauncher.app/Contents/Resources/ScriptsX/scriptsx.lua
+++
@@ -1,9 +0,0 @@
-dofile('ScriptsX/binded_attr.lua')
-
--- print(package.path)
--- table.insert(package.path, '/Users/secondatke/Documents/branches/MarXsCube/Externals/ScriptsX')
-package.path = package.path .. ';./ScriptsX/?.lua'
--- print(package.path)
-
--- bindedattr = require 'binded_attr'
-bindedattr = _binded_attr
