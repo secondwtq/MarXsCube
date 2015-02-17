010f17f9d317fe191c04dd5772ecb7edcecc6cf6
secondwtq <lovejay-lovemusic@outlook.com>
2015-02-17 22:52:49 +0800

    Update.

--- org/utils/jitit.py
+++ org/utils/jitit.py
@@ -9,6 +9,11 @@ PATH_OUTPUT = '.'
 import os
 import os.path
 import subprocess
+import argparse
+
+parser_main = argparse.ArgumentParser(description='LuaJit automated compiler for object file to link into exectuable.')
+parser_main.add_argument('-s', '--source', dest='source', help='Directory containing the source.', default='.')
+parser_main.add_argument('-o', '--output', dest='output', help='Directory to storage the output files.', default='.')
 
 def merge_path_single(root, files):
 	return [os.path.join(root, filename) for filename in files]
@@ -25,7 +30,15 @@ def pair_scripts(source):
 				for srcfile in source]
 
 def main():
+	args = parser_main.parse_args()
+	PATH_SOURCE = args.source
+	PATH_OUTPUT = args.output
+
 	file_pairs = pair_scripts(list_files(PATH_SOURCE))
+	if not len(file_pairs):
+		print('No script here, exiting.')
+		return 0
+
 	for source, output in file_pairs:
 		output_path = os.path.join(PATH_OUTPUT, output)
 		command = '%s -b %s %s' % (EXEC, source, output_path)
@@ -35,7 +48,9 @@ def main():
 		print(p.stdout.read())
 
 	print('Done.')
-
+	return 0
 
 if __name__ == '__main__':
 	status = main()
+	import sys
+	sys.exit(status)
